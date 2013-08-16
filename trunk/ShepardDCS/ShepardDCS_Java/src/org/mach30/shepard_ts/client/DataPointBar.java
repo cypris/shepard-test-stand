f7b2245f77a70d4060f559193d361fdf27d6d043
csigman <csigman@d17c5624-bd3d-11e0-be83-6bdb0c71f5e5>
2013-08-16 03:54:29 +0000

    0.1 revision of the Java based GUI

---
+++ trunk/ShepardDCS/ShepardDCS_Java/src/org/mach30/shepard_ts/client/DataPointBar.java
@@ -0,0 +1,136 @@
+package org.mach30.shepard_ts.client;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class DataPointBar extends JPanel
+{
+
+  private float maxVal = 0;
+  private float minVal = 0;
+  private float val = 0;
+  private String datapointLabel = "";
+  private String barLabel = "";
+  private int barHeight = 0;
+
+  public DataPointBar(float min, float max)
+  {
+    minVal = min;
+    maxVal = max;
+    
+    initComponents();
+  }
+  
+  private void initComponents()
+  {
+    setLayout(null);
+    
+    setBackground(Color.WHITE);
+    
+    this.revalidate();
+    
+    setVisible(true);
+  }
+  
+  @Override
+  public Dimension getPreferredSize()
+  {
+    return new Dimension(40, 570);
+  }
+  
+  @Override
+  public Dimension getMaximumSize()
+  {
+    return new Dimension(50, 4000);
+  }
+  
+  @Override
+  public void paint(Graphics g)
+  {
+    super.paint(g);
+    
+    int width = getWidth();
+    int height = getHeight();
+    
+    g.setColor(Color.BLACK);
+    g.drawRect(8, 8, width - 16, height - 32);
+
+    g.drawString(barLabel, barLabel.length() * 5 / 2, height - 10);
+    
+    g.setColor(Color.BLUE);
+    g.fillRect(9, height + 8 - barHeight, width - 17, barHeight - 32);
+
+    g.setFont(Font.getFont(Font.MONOSPACED));
+    ((Graphics2D)g).rotate(1.57079633);
+    g.drawString(datapointLabel, 10, -12);
+  }
+  
+  @Override
+  public void setBounds(int x, int y, int width, int height)
+  {
+    super.setBounds(x, y, width, height);
+    
+    positionDatapointLabel();
+  }
+  
+  @Override
+  public void setSize(Dimension d)
+  {
+    super.setSize(d);
+    setSize(d.width, d.height);
+  }
+  
+  @Override
+  public void setSize(int width, int height)
+  {
+    super.setSize(width, height);    
+    positionDatapointLabel();
+  }
+  
+  public void setLabel(String label)
+  {
+    barLabel = label;
+  }
+  
+  public void setData(float point)
+  {
+    if (point > maxVal)
+    {
+      val = maxVal;
+    }
+    else if (point < minVal)
+    {
+      val = minVal;
+    }
+    else
+    {
+      val = point;
+    }
+    
+    datapointLabel = Float.toString(point);
+    
+    positionDatapointLabel();
+    
+    repaint();
+  }
+  
+  private void positionDatapointLabel()
+  {
+    int constraint = getHeight() - 40;
+    constraint *= val;
+    float height = constraint / maxVal;
+    barHeight = Math.round(height) + 33;
+    
+    this.revalidate();
+  }
+  
+
+  private static final long serialVersionUID = -166246168499362604L;
+  
+}
---
+++ trunk/ShepardDCS/ShepardDCS_Java/src/org/mach30/shepard_ts/client/ShepardDataPanel.java
@@ -0,0 +1,104 @@
+package org.mach30.shepard_ts.client;
+
+import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.IAxis;
+import info.monitorenter.gui.chart.IAxis.AxisTitle;
+import info.monitorenter.gui.chart.IRangePolicy;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.rangepolicies.RangePolicyFixedViewport;
+import info.monitorenter.gui.chart.traces.Trace2DLtd;
+import info.monitorenter.util.Range;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+public class ShepardDataPanel extends JPanel
+{
+  
+  private ITrace2D trace = null;
+  
+  private Chart2D chart = null;
+  private DataPointBar maxBar = null;
+  private DataPointBar minBar = null;
+  private DataPointBar avgBar = null;
+  
+  private float max = Float.MIN_VALUE;
+  private float min = Float.MAX_VALUE;
+  private float sum = 0.0f;
+  private float numPoints = 0.0f;
+
+  public ShepardDataPanel(String yAxisTitle, float yAxisMax)  
+  {
+    initComponents(yAxisMax);
+    setYAxisInfo(yAxisTitle, yAxisMax);
+  }
+
+  private void initComponents(float yAxisMax)
+  {
+    BoxLayout layout = new BoxLayout(this, BoxLayout.LINE_AXIS);
+    setLayout(layout);
+    
+    chart = new Chart2D();
+    trace = new Trace2DLtd(10000); 
+    trace.setColor(Color.BLUE);
+    chart.addTrace(trace);
+    chart.setPreferredSize(new Dimension(800, 570));
+    add(chart);
+    
+    maxBar = new DataPointBar(0.0f, yAxisMax);
+    maxBar.setLabel("Max");
+    add(maxBar);
+    
+    minBar = new DataPointBar(0.0f, yAxisMax);
+    minBar.setLabel("Min");
+    add(minBar);
+    
+    avgBar = new DataPointBar(0.0f, yAxisMax);
+    avgBar.setLabel("Avg");
+    add(avgBar);
+
+    setVisible(true);
+  }
+  
+  public void setYAxisInfo(String yAxisTitle, float yAxisMax)
+  {  
+    chart.getAxesXBottom().get(0).setAxisTitle(new AxisTitle("Time (ms)"));
+    IAxis<?> yAxis = chart.getAxesYLeft().get(0);
+    yAxis.setAxisTitle(new AxisTitle(yAxisTitle));
+    IRangePolicy rangePolicy = new RangePolicyFixedViewport(new Range(0, yAxisMax));
+    yAxis.setRangePolicy(rangePolicy);
+  }
+
+  public void clear()
+  {
+    trace.removeAllPoints();
+    max = 0;
+    min = 42.0f;
+  }
+
+  public void addPoint(float val, long timestamp)
+  {
+    trace.addPoint(timestamp, val);
+    
+    if (val > max)
+    {
+      max = val;
+      maxBar.setData(val);
+    }
+    else if (val < min)
+    {
+      min = val;
+      minBar.setData(val);
+    }
+    
+    ++numPoints;
+    sum += val;
+    avgBar.setData(sum / numPoints);
+  }
+  
+  private static final long serialVersionUID = -7559126468381303833L;
+  
+}
